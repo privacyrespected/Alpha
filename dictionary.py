a8a31b5c60097a36a8780ee9e4b84d69efcfdf08
nuggetcatsoftware <nuggetcatsoftware@gmail.com>
2021-04-15 16:51:58 +0800

    updated dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,35 +1,11 @@
-import urllib.request
 from bs4 import BeautifulSoup
-
-word = input('Enter the word to find the meaning : ')
-
-url = "https://www.vocabulary.com/dictionary/" + word + ""
-htmlfile = urllib.request.urlopen(url)
-soup = BeautifulSoup(htmlfile, 'lxml')
-
-soup1 = soup.find(class_="short")
-
-try:
-    soup1 = soup1.get_text()
-except AttributeError:
-    print('Cannot find such word! Check spelling.')
-    exit()
-
-# Print short meaning
-print ('-' * 25 + '->',word,"<-" + "-" * 25)
-print ("SHORT MEANING: \n\n",soup1)
-print ('-' * 65)
-
-# Print long meaning
-soup2 = soup.find(class_="long")
-soup2 = soup2.get_text()
-print ("LONG MEANING: \n\n",soup2)
-
-print ('-' * 65)
-
-# Print instances like Synonyms, Antonyms, etc.
-soup3 = soup.find(class_="instances") 
-txt = soup3.get_text()
-txt1 = txt.rstrip()
-
-print (' '.join(txt1.split()))
+import requests
+def dictionary(word):
+    word = str(word)
+    url = "http://dictionary.cambridge.org/dictionary/british/" + word.lower()
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content)
+    word = soup.find("span", {"class": "pos"}).text
+    definition = soup.find("span", {"class": "def"}).text
+    print(word)
+    print(definition)
