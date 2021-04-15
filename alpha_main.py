03be5f9297cc59e7c0baffc4eb1a1dda2af014c3
nuggetcatsoftware <nuggetcatsoftware@gmail.com>
2021-04-15 18:45:11 +0800

    News implementation (Function made)

--- alpha_main.py
+++ alpha_main.py
@@ -36,6 +36,8 @@ from win10toast import ToastNotifier
 from bs4 import BeautifulSoup as bs
 import requests
 import time
+import requests
+from bs4 import BeautifulSoup
 #syntax alarm(hour, minute)
 try:
     import webbrowser
@@ -499,7 +501,75 @@ def alarm(hour, minute):
 
         #Save the dataframe as a csv (if already present, will overwrite the previous one)
         df.to_csv('tune_parameters.csv',index=False)
+def getnews():
+    
+    # url definition
+    url = "https://www.theguardian.com/uk"
+    
+    # Request
+    r1 = requests.get(url)
+    r1.status_code
 
+    # We'll save in coverpage the cover page content
+    coverpage = r1.content
+
+    # Soup creation
+    soup1 = BeautifulSoup(coverpage, 'html5lib')
+
+    # News identification
+    coverpage_news = soup1.find_all('h3', class_='fc-item__title')
+    len(coverpage_news)
+    
+    number_of_articles = 5
+
+    # Empty lists for content, links and titles
+    news_contents = []
+    list_links = []
+    list_titles = []
+
+    for n in np.arange(0, number_of_articles):
+
+        # We need to ignore "live" pages since they are not articles
+        if "live" in coverpage_news[n].find('a')['href']:  
+            continue
+
+        # Getting the link of the article
+        link = coverpage_news[n].find('a')['href']
+        list_links.append(link)
+
+        # Getting the title
+        title = coverpage_news[n].find('a').get_text()
+        list_titles.append(title)
+
+        # Reading the content (it is divided in paragraphs)
+        article = requests.get(link)
+        article_content = article.content
+        soup_article = BeautifulSoup(article_content, 'html5lib')
+        body = soup_article.find_all('div', class_='content__article-body from-content-api js-article__body')
+        x = body[0].find_all('p')
+
+        # Unifying the paragraphs
+        list_paragraphs = []
+        for p in np.arange(0, len(x)):
+            paragraph = x[p].get_text()
+            list_paragraphs.append(paragraph)
+            final_article = " ".join(list_paragraphs)
+
+        news_contents.append(final_article)
+
+    # df_features
+    df_features = pd.DataFrame(
+         {'Content': news_contents 
+        })
+
+    # df_show_info
+    df_show_info = pd.DataFrame(
+        {'Article Title': list_titles,
+         'Article Link': list_links,
+         'Newspaper': 'The Guardian'})
+
+    
+    return (df_features, df_show_info)
 def alphamain():
     while True:
         query = listen().lower()
