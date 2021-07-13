06a108ec80d790e1be4453223ec44ebf455e886a
nuggetcatsoftware <nuggetcatsoftware@gmail.com>
2021-07-13 15:07:31 +0800

    fix

--- usersetting/usersettings.py
+++ usersetting/usersettings.py
@@ -39,21 +39,11 @@ def usersettingwrite(username, usercity, user_gender, userdob, useremail, userem
                     "userheight":"null",
                     "usersport":"null",
                     "userhobby":"null",
-                    "userinterest":"null"
+                    "userinterest":"null",
+                    "userpersonaldiscordbottoken":"null"
                 }
-            }
-            medic_data={
-                "medic_data":{
-                    "username":username
-                    #continue writing data in alpha_main.py
-                }
-            }
-            personal_discord_bot_token={
-                "null"
             }
             json.dump(data, f, ensure_ascii=False, indent=4)
-            json.dump(medic_data, f, ensure_ascii=False, indent=4)
-            json.dump(personal_discord_bot_token, f,ensure_ascii=False, indent=4)
     except Exception as e:
         print(e)
 if sys.platform in ['win32', 'win64'] and int(platform.release()) >= 10:
