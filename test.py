b9a1a3cf039fb2144604c27ae3a0f1f166e886be
nuggetcatsoftware <nuggetcatsoftware@gmail.com>
2021-07-13 21:04:24 +0800

    its working holy sheet

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import json
 with open("data.json", "r") as read_file:
     data = json.load(read_file) 
-    print(data)
+    e=data["main_user_data"]
+    f=e["username"]
+    print(e)
+
+    print(f)
