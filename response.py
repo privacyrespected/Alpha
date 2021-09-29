b2bf56371ac86f0443b60a9dd1be7904b9e53b54
nuggetcatsoftware <nuggetcatsoftware@gmail.com>
2021-09-29 20:50:12 +0800

    Added custom response for developers

---
+++ response.py
@@ -0,0 +1,105 @@
+positivesigns=[
+    "yes",
+    "sure",
+    "true",
+    "why not",
+    "ok",
+    "okay",
+    "positive",
+    "that would be great",
+    "that'd be great",
+    "that would be nice",
+    "that'd be nice",
+    "that do be great",
+    "that do be nice",
+    "if that's possible",
+    "if that is possible"
+                ]
+negativesigns=[
+    "no",
+    "no thanks",
+    "im fine",
+    "nah",
+    "negative"
+]
+thanks=[
+    "thanks",
+    "thank you",
+    "that's nice"
+]
+asking_for_opinion=[
+    "what do you think about",
+    "what are your thoughts about",
+    "what is your opinion towards",
+    "your opinion",
+    "your thoughts"
+]
+opinion_reply=[
+    "Well, I think that",
+    "In my opinion",
+    "To be honest"
+]
+alarm_trigger=[
+    "Set an alarm",
+    "Wake me up",
+    "Remind me"
+]
+alarm_trigger_aims=[
+    "to",
+    "in order to"
+]
+sysinfo=[
+    "system information",
+    "about this PC",
+    "Open full system analysis",
+    "Can i see my computer's stats?",
+    "Can i view my computer's statistics?",
+    "system statistics",
+    "system stats",
+    "system information",
+    "information regarding my PC",
+    "information regarding my computer",
+    "PC stats",
+    "PC statistics",
+    "PC info",
+    "PC information"
+]
+meaning=[
+    "meaning",
+    "definition"
+]
+happy_response=[
+    "I'm happy to hear that",
+    "That's good news!",
+    "Nice."
+]
+sad_response=[
+    "I'm sorry to hear that.",
+    "That's not good",
+    "What can I help?"
+]
+understandnt=[
+    "Sorry, I do not understand",
+    "Apologies, I do not understand",
+    "Can you repeat that, please",
+    "Sorry I do not have the ability to solve this problem"
+]
+thankyouresponse=[
+    "Thanks",
+    "Thank you very much",
+    "At your service"
+]
+calculate_triggger=[
+    "Calculate",
+    "evaluate",
+    "what is",
+]
+swearword=[
+    "fuck",
+    "shit",
+    "fucker",
+    "motherfucker"
+    #we need more here!!
+]
+thenonoword="nigger"
+thenonowordresponse="The N word you just said is considered extremely offensive. I would like to urge you to calm yourself down or I will shut myself down."
