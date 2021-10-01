2ece726b5418fc157c06ed1a160bf9872ded1da2
nuggetcatsoftware <nuggetcatsoftware@gmail.com>
2021-10-01 19:54:09 +0800

    added imports for functions.py

---
+++ functions.py
@@ -0,0 +1,50 @@
+def reporterror(errorr, suggest):
+    print(errorr + suggest)
+#import modules
+import discord
+from discord import channel
+from discord.ext import commands
+from sys import excepthook
+import pyttsx3
+import os
+import wikipedia
+import time
+import os
+import os.path
+from os import error, path
+import eel
+from datetime import date
+import speech_recognition as sr
+import datetime
+import datetime
+import os
+import time
+import random
+import csv
+from pygame import mixer
+import pandas as pd
+import numpy as np
+from threading import Thread
+import platform
+import discord
+import psutil
+from win10toast import ToastNotifier
+from bs4 import BeautifulSoup as bs
+import requests
+import time
+import requests
+from bs4 import BeautifulSoup
+import sys
+import operator
+import json
+from response import *
+from functions import *
+#syntax alarm(hour, minute)
+try:
+    import webbrowser
+except ImportError:
+    reporterror("Module webbrowser not found", "Legit, fix your damn pc")
+try:
+    from googlesearch import search
+except ImportError:
+    reporterror("module google not found", "Check pip installation")
