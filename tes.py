fa1185f19ec21ff2ad401b394674b77248c6ec9e
nuggetcatsoftware <nuggetcatsoftware@gmail.com>
2021-10-01 18:17:58 +0800

    testing list str logic

---
+++ tes.py
@@ -0,0 +1,12 @@
+list=[
+    "1",
+    "2",
+    "3",
+    "4"
+]
+while True:
+    word =input(">> ")
+    if word in list:
+        print("The word is in the list!")
+    else:
+        print("The word is not in the list!")
