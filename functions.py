7f8470f5572f53398684a8489db7efee539db67e
nuggetcatsoftware <nuggetcatsoftware@gmail.com>
2021-12-13 01:31:22 +0000

    launching debug version

--- functions.py
+++ functions.py
@@ -22,21 +22,7 @@ def speak(audio):
     engine.runAndWait()
 #speech recognition
 def listen():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening>>>")
-        r.pause_threshold = 1
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source)
-
-    try:
-        print("Recognizing: ")
-        query = r.recognize_google(audio, language='en-in')
-        print(f"User said {query}\n")
-    except Exception as e:
-        print(e)
-        print("Audio not heard, plesae try again")
-        return "None"
+    query=input("Input command to start debugging: ")
     return query
 #wikipedia search
 def wikipedia(query):       
