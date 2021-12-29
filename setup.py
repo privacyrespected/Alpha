9fc46ecc39e5aa2cce2fc2d0880aaf9deebb686d
nuggetcatsoftware <nuggetcatsoftware@gmail.com>
2021-12-29 12:13:43 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -16,17 +16,17 @@ setup(
     packages=['audio'],
     install_requires=[
         'requests>=2.5.3',
-        'ChatterBot==1.0.8',
-        'chatterbot-corpus==1.2.0',
-        'win10toast==0.9',
-        'wikipedia==1.4.0',
-        'pyttsx3==2.90',
-        'Eel==0.12.4',
-        'SpeechRecognition==3.8.1',
-        'pprintpp==0.4.0',
-        'PyAutoGUI==0.9.52',
-        'psutil==5.7.3',
-        'playsound==1.2.2'
+        'ChatterBot>=1.0.8',
+        'chatterbot-corpus>=1.2.0',
+        'win10toast>=0.9',
+        'wikipedia>=1.4.0',
+        'pyttsx3>=2.90',
+        'Eel>=0.12.4',
+        'SpeechRecognition>=3.8.1',
+        'pprintpp>=0.4.0',
+        'PyAutoGUI>=0.9.52',
+        'psutil>=5.7.3',
+        'playsound>=1.2.2'
     ],
     license=open('LICENSE').read()
 )
